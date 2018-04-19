bae52a6fdb571cb10e6495c7f9f5a5b08a2d1534
Bjørn Asle Taranger <bjorn.taranger@gmail.com>
2018-04-19 23:28:42 +0200

    updated some dependencies
    Signed-off-by: Bjørn Asle Taranger <bjorn@bjrns-mbp.lan>

--- project.clj
+++ project.clj
@@ -3,8 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/test.check "0.5.7"]
-                 [expectations "2.0.7"]]
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [org.clojure/test.check "0.9.0"]
+                 [expectations "2.1.9"]]
   :plugins [[lein-autoexpect "1.2.2"]
             [lein-ancient "0.5.5"]])
